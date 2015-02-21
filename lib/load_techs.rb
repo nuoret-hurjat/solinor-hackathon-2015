5c56982b96669400aa66ea58f183d9b7d1bf2357
Pavel <feiseater@ubuntu.(none)>
2015-02-21 11:49:02 +0200

    tekkilogot

--- lib/load_techs.rb
+++ lib/load_techs.rb
@@ -5,10 +5,11 @@ class LoadTechs
   end
 
   def self.lue_json
-    url = "https://raw.githubusercontent.com/ElbertF/Wappalyzer/master/share/apps.json"
+    url = "https://raw.githubusercontent.com/ElbertF/Wappalyzer/master/src/apps.json"
+    logo_url = "https://raw.githubusercontent.com/ElbertF/Wappalyzer/master/src/icons/"
     haku = read_json_techs(url)
     haku["apps"].each do |k|
-      Technology.create name:k[0], url:k[1]["website"]
+      Technology.create name:k[0], url:k[1]["website"], logo:"#{logo_url}#{k[0]}.png"
     end
   end
 end
