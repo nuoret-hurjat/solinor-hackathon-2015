0b6873dce5aeae3e72f81cba0b36696db264768c
Ville-Veikko Saari <vrsaari@dhcp-asv-90.helsinki.fi>
2015-02-21 05:52:45 +0200

    timeout crawleriin

--- lib/fetch_pages.rb
+++ lib/fetch_pages.rb
@@ -2,23 +2,27 @@ class FetchPages
   def self.get_url_from_file_and_crawl(file)
     IO.foreach(file) do |line|
       begin
-      rivi = line.gsub("\n", '').squeeze(' ')
-      url = "http://#{rivi}"
-      logo = GetImagesApi.get_images_mechanize(url)
-      logo = "tyhjä" if logo.nil?
-      techs = WappalyzerRb::Detector.new(url).analysis
-      urli = Domainatrix.parse(url)
-      if not Company.find_by name:urli.domain
-        firma = Company.create name:urli.domain, logo:logo, url:url, admin_edited:false
-      else
-        firma = Company.find_by name:urli.domain
-      end
-      techs.each do |t|
-        tech = Technology.find_by name:t
-        firma.technologies << tech unless firma.technologies.include?(tech)
-      end
-      rescue
+        status = Timeout::timeout(2) {
 
+          rivi = line.gsub("\n", '').squeeze(' ')
+          url = URI::encode("http://#{rivi}")
+          logo = GetImagesApi.get_images_mechanize(url)
+          logo = "tyhjä" if logo.nil?
+          techs = WappalyzerRb::Detector.new(url).analysis
+          urli = Domainatrix.parse(url)
+          if not Company.find_by name:urli.domain
+            firma = Company.create name:urli.domain, logo:logo, url:url, admin_edited:false
+          else
+            firma = Company.find_by name:urli.domain
+          end
+          techs.each do |t|
+            tech = Technology.find_by name:t
+            firma.technologies << tech unless firma.technologies.include?(tech)
+          end
+
+        }
+      rescue
+        puts("APUA")
       end
     end
   end
