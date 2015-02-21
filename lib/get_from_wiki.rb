a3b27f81ee2477dd0beb64ae929cdc3b6df0702b
Ville-Veikko Saari <vrsaari@dhcp-asv-90.helsinki.fi>
2015-02-21 14:01:40 +0200

    GetFromWiki fixes

--- lib/get_from_wiki.rb
+++ lib/get_from_wiki.rb
@@ -1,17 +1,24 @@
 class GetFromWiki
   def self.get_from_wiki(haku)
-    url = "http://dbpedia.org/data/#{haku.capitalize}.json"
+    url = URI::encode("http://dbpedia.org/data/#{haku.capitalize}.json")
     r = RestClient.get url
     h = JSON.parse(r)
-    n = Hash.new
+    return nil if h.size == 0
+    unless h[URI::encode("http://dbpedia.org/resource/#{haku.capitalize}")]["http://dbpedia.org/ontology/abstract"].nil?
+      puts h
+      puts h.size
+      puts h[URI::encode("http://dbpedia.org/resource/#{haku.capitalize}")]["http://dbpedia.org/ontology/abstract"]
+      n = Hash.new
 
-
-    
-    h["http://dbpedia.org/resource/#{haku.capitalize}"]["http://dbpedia.org/ontology/abstract"].each do |j|
-      if j.to_h["lang"] == "en"
-        n=j.to_h
+      h[URI::encode("http://dbpedia.org/resource/#{haku.capitalize}")]["http://dbpedia.org/ontology/abstract"].each do |j|
+        if j.to_h["lang"] == "en"
+          n=j.to_h
+        end
       end
+      n["value"]
+    else
+      return nil
     end
-    n["value"]
   end
 end
+
